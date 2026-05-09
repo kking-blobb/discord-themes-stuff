69c34cb02d445a12cc79930a50a2aac55ae5c7e2
kking-blobb <kkingblob@gmail.com>
2026-05-09 14:56:04 -0500

    adding element and style files

---
+++ plugins/ChatBarLayout/ChatBarSettings.tsx
@@ -0,0 +1,19 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import "./style.css";
+
+import React from "react";
+
+export default function ChatBarSettings() {
+    return (
+        <div className="chatBarButtonPlacement wrapper__72c38 ">
+            <div className="buttonSlotContainer">
+                <div className="buttonSlot"></div>
+            </div>
+        </div>
+    );
+}
